577a7f4254fd366f6a7e2abaf097182ea90fe2ad
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-12 12:45:43 +0530

    first commit

---
+++ src/test/java/com/nest/NestApp_Backend/NestAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.NestApp_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
