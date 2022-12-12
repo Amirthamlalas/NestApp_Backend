3345593093c91218a8df7da9fe134d6ca202fce6
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-12 23:34:29 +0530

    added counter

--- src/main/java/com/nest/NestApp_Backend/controller/SecurityController.java
+++ src/main/java/com/nest/NestApp_Backend/controller/SecurityController.java
@@ -1,7 +1,7 @@
 package com.nest.NestApp_Backend.controller;
 
-import com.nest.NestApp_Backend.dao.LeaveDao;
-import com.nest.NestApp_Backend.model.LeaveApplication;
+import com.nest.NestApp_Backend.dao.VisitorDao;
+import com.nest.NestApp_Backend.model.VisitorLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,20 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
-public class LeaveController {
+public class SecurityController {
     @Autowired
-    private LeaveDao dao;
-
+    private VisitorDao dao;
     @CrossOrigin(origins = "*")
-    @PostMapping(path = "/applyleave",consumes = "application/json",produces = "application/json")
-    public HashMap<String, String> applyLeave(@RequestBody LeaveApplication l){
-        dao.save(l);
+    @PostMapping(path = "/visitorlog",consumes = "application/json",produces = "application/json")
+    public HashMap<String, String> visitorlog(@RequestBody VisitorLog v){
+        dao.save(v);
         HashMap<String,String>map=new HashMap<>();
         map.put("status","success");
         return map;
+
     }
-
-
-
 
 }
