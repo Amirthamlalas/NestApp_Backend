8c104e674b4cc20c45f80c292f8c252706965741
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-12 14:53:01 +0530

    added delete

--- src/main/java/com/nest/NestApp_Backend/dao/EmployeeDao.java
+++ src/main/java/com/nest/NestApp_Backend/dao/EmployeeDao.java
@@ -1,9 +1,11 @@
 package com.nest.NestApp_Backend.dao;
 
 import com.nest.NestApp_Backend.model.Employees;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,4 +13,9 @@ public interface EmployeeDao extends CrudRepository<Employees,Integer> {
 
     @Query(value = "SELECT `id`, `designation`, `email`, `empid`, `name`, `password`, `phoneno`, `salary`, `username` FROM `employees` WHERE `name`= :name",nativeQuery = true)
     List<Employees>searchEmployee(@Param("name")String name);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `employees` WHERE `id`= :id",nativeQuery = true)
+    void deleteEmployee(@Param("id")Integer id);
 }
