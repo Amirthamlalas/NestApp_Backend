4f1386cecd4d3f27641caba3b84fd170512d7e7d
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-12-13 10:00:44 +0530

    ADDED COUNTER

--- src/main/java/com/nest/NestApp_Backend/controller/LeaveController.java
+++ src/main/java/com/nest/NestApp_Backend/controller/LeaveController.java
@@ -3,12 +3,11 @@ package com.nest.NestApp_Backend.controller;
 import com.nest.NestApp_Backend.dao.LeaveDao;
 import com.nest.NestApp_Backend.model.LeaveApplication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class LeaveController {
@@ -20,6 +19,28 @@ public class LeaveController {
     public HashMap<String, String> applyLeave(@RequestBody LeaveApplication l){
         dao.save(l);
         HashMap<String,String>map=new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(path = "/viewallleave")
+    public List<Map<String,String>>viewallLeave(){
+        return(List<Map<String,String>>)dao.viewLeave();
+
+
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(value = "/update",consumes = "application/json",produces = "application/json")
+    public Map<String,String> UpdateStatus(@RequestBody LeaveApplication l)
+    {
+        String empid=String.valueOf(l.getEmpid());
+        String status=l.getStatus().toString();
+        System.out.println(empid);
+        System.out.println(status);
+        dao.updateStatus(l.getEmpid(),l.getStatus());
+        HashMap<String,String> map=new HashMap<>();
         map.put("status","success");
         return map;
     }
--- src/main/java/com/nest/NestApp_Backend/dao/LeaveDao.java
+++ src/main/java/com/nest/NestApp_Backend/dao/LeaveDao.java
@@ -1,7 +1,22 @@
 package com.nest.NestApp_Backend.dao;
 
 import com.nest.NestApp_Backend.model.LeaveApplication;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Map;
 
 public interface LeaveDao extends CrudRepository<LeaveApplication,Integer> {
+
+    @Query(value = "SELECT  e.`empid`, e.`name`,l.leavetype,l.status,l.from_data,l.to_date  FROM `employees`e JOIN leaveapp l ON e.`empid`=l.empid",nativeQuery = true)
+    List<Map<String,String>> viewLeave();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE `leaveapp` SET `status`= :status WHERE `empid`=:empid",nativeQuery = true)
+    void updateStatus(@Param("empid") Integer empid,@Param("status")String status);
 }
